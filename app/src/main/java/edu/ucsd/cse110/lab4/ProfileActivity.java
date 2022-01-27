78609671fe0000c84dffe1ad5223ae8628e7ebd0
XunhaoYangRobert <yang980666936@outlook.com>
2022-01-27 00:17:23 -0800

    update part2

--- app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
@@ -2,8 +2,11 @@ package edu.ucsd.cse110.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -11,9 +14,50 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        loadProfile();
     }
 
-    public void onGoBackClicked(View view) {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveProfile();
+    }
+
+    public void onGoBackClicked(View view){
+
         finish();
     }
+
+    public void loadProfile() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        EditText nameText = findViewById(R.id.name_textview);
+        EditText statusText = findViewById(R.id.status_textview);
+
+
+
+
+
+        nameText.setText(preferences.getString("name", ""));
+        statusText.setText(preferences.getString("status", ""));
+
+    }
+
+    public void saveProfile() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        EditText nameText = findViewById(R.id.name_textview);
+        EditText statusText = findViewById(R.id.status_textview);
+        editor.putString("name", nameText.getText().toString());
+        editor.putString("status", statusText.getText().toString());
+
+
+
+
+        editor.apply();
+
+    }
+
+
+
+
 }
